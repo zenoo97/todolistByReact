bd428d987d40f3401104bd66a2359c1b6dcd799f
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2023-02-03 16:12:06 +0900

    [Update] todo 기본 틀

---
+++ todolist/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]); // todo리스트를 담아둘 배열 state
+
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo(""); // input창을 비우는 것
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    console.log(toDos);
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
